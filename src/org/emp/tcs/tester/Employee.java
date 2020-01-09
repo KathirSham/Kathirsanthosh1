713f8c355a92f2c069e3609fa8ee62e33a47f0a8
KathirSham <kathir3012@gmail.com>
2020-01-09 13:27:43 +0530

    The Code is installed Successfully

---
+++ src/org/emp/tcs/tester/Employee.java
@@ -0,0 +1,8 @@
+package org.emp.tcs.tester;
+
+public class Employee {
+	public void empid() {
+		System.out.println("15 lakhs ");
+	}
+
+}
---
+++ src/org/emp/tcs/tester/Employee1.java
@@ -0,0 +1,14 @@
+package org.emp.tcs.tester;
+
+public class Employee1 extends Employee{
+	@Override
+	public void empid() {
+		
+		System.out.println("20 lakhs");
+		
+	}
+public static void main(String[] args) {
+	Employee1 a = new Employee1();
+	a.empid();
+}
+}
